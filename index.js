6c794e14626ba166044ee769c1d218efaa50a7c9
Derrick Craven <dcravenus@gmail.com>
2019-02-08 15:49:38 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const proxy = require("express-http-proxy");
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+
+express()
+  .use("/tvmaze", proxy("http://api.tvmaze.com"))
+  .use("/pixel-color", express.static(path.join(__dirname, "node_modules/pixel-color")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => res.render("pages/index"))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
